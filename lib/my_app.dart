55db7f6629b7c06616dadebb4410d2b7b14ef248
muhammad-fathy <m.fathy@ryze.com.sa>
2025-10-16 12:02:16 +0300

    remove unness import

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 import 'package:whatsapp_workflow_mobileapp/config/router/go_router_config.dart';
 import 'package:whatsapp_workflow_mobileapp/features/home/presentation/bloc/home_bloc.dart';
 import 'package:whatsapp_workflow_mobileapp/injectable.dart';
